2347019c53aa09324421a9042338c930f18c694c
VictorAJM <victorajm@outlook.com>
2021-07-11 18:24:46 -0500

    Hola

--- Codeforces Round #731 (Div. 3)/A.cpp
+++ Codeforces Round #731 (Div. 3)/A.cpp
@@ -16,14 +16,23 @@ typedef vector<ii> vii;
 
 void solve()
 {
+    int n,pa,pb,da,db,fa,fb;
 
+    cin >> pa >> pb;
+    cin >> da >> db;
+    cin >> fa >> fb;
+    int r = abs(pa-da) + abs(pb-db);
+    if (pa == da && fa == da && ((pb <= fb && fb <= db) || (db <= fb && fb <= pb))) r += 2;
+    else if (pb == db && fb == db && ((pa <= fa && fa <= da) || (da <= fa && fa <= pa))) r += 2;
+    cout << r << "\n";
+    return; 
 }
 
 int main()
 {
     fast_io;
     int t = 1;
-    // cin >> t;
+    cin >> t;
     while (t--) {
         solve();
     }
