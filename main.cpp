ef728c50b24f5a9669a06a33c3bdaccdd79b1064
VictorAJM <86992493+VictorAJM@users.noreply.github.com>
2021-08-30 20:24:01 -0500

    morido

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int n,m;
-
-int main(){
-	cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
-	cin >> n >> m;
-	cout << (n*m)/2;
-	return 0;
+#include <stdio.h>
+#include <math.h>
+float a;
+int main() {
+    printf("Introduzca una cantidad en radianes.");
+    scanf("%f",&a);
+    float b = a * 180.0 / acos(-1.0);
+    printf("%.2f radianes es igual a %.2f grados.",a,b);
+    return 0;
 }
